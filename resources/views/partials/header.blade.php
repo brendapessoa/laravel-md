ecc6c82ce13c6402ccff9d9b1fd7e7150a642b77
BrendaPessoa <brenda.nunys@gmail.com>
2017-04-13 15:59:56 +0100

    Customizing User Authentication

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,22 +7,22 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a href="{{ route('blog.index') }}" class="navbar-brand">Laravel Guide</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <a href="{{ route('blog.index') }}" class="navbar-brand">Laravel Guide</a>
+
             <ul class="nav navbar-nav">
                 <li><a href="{{ route('blog.index') }}">Blog</a></li>
                 <li><a href="{{ route('other.about') }}">About</a></li>
-                {{--@if (Auth::guest())--}}
+                @if(!Auth::check())
                 <li><a href="{{ url('/login') }}">Login</a></li>
                 <li><a href="{{ url('/register') }}">Register</a></li>
-                {{--@else--}}
+                @else
                 <li><a href="{{ route('admin.index') }}">Posts</a></li>
                 <li>
                     <a href="{{ url('/logout') }}"
                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                       document.getElementById('logout-form').submit();">
                         Logout
                     </a>
 
@@ -30,6 +30,7 @@
                         {{ csrf_field() }}
                     </form>
                 </li>
+                    @endif
             </ul>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,7 @@ Route::group(['prefix' => 'admin'], function() {
 });
 Auth::routes();
 
+Route::post('login', [
+    'uses' => 'SigninController@signin',
+    'as' => 'auth.signin'
+]);
